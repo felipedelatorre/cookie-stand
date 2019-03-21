a1a1e2b00fd224484d1d7feaa96915b8358c2d65
DukeOfEtiquette <duquetteadam@gmail.com>
2019-03-21 14:12:28 -0700

    Add comments throughout code as suggestions for changes or to consider for next time

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cookie-stand
 Salmon Cookie Shop
+
+Don't overlook this README file, very important first impression when someone is viewing your repository. - Adam
--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 // Define Function
 //---------------------------
 
+var HOURS = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm'];
+
 //Renders header row of times
 var renderRowOfTimes = function(parentElement) {
   var tr = document.createElement('tr');
@@ -26,6 +28,32 @@ var renderRowOfTimes = function(parentElement) {
   tr.appendChild(th);
 };
 
+// function Element(type, content, attr, attrValue){
+//   this.type = type;
+//   this.content = content;
+//   this.attr = attr;
+//   this.attrValue = attrValue;
+// }
+
+// Element.prototype.appendElement = function(parentNode){
+//   var newNode = document.createElement(this.type);
+
+//   if(this.attr){
+//     newNode.setAttribute(this.attr, this.attrValue);
+//   }
+
+//   if(this.content){
+//     newNode.textContent = this.content;
+//   }
+
+//   parentNode.appendChild(newNode);
+// };
+
+// var element = new Element('tr', null, 'id', 'totals');
+// element.appendElement(parentElement);
+
+// element = new Element('td', 24);
+
 // Renders last row of totals
 var renderRowOfTotals = function(parentElement, locationStores) {
   var tr = document.createElement('tr');
@@ -36,11 +64,12 @@ var renderRowOfTotals = function(parentElement, locationStores) {
   td.textContent = 'Totals';
   tr.appendChild(td);
 
+  // var key = 'cookiesPurchasePerHour'; // Then use key on for loop
   for (var i = 0; i < locationStores[0]['cookiesPurchasePerHour'].length; i++) {
     var storeTotals = 0;
     td = document.createElement('td');
     for (var j = 0; j < locationStores.length; j++) {
-      storeTotals += locationStores[j].cookiesPurchasePerHour[i];    
+      storeTotals += locationStores[j].cookiesPurchasePerHour[i];
     }
     td.textContent = storeTotals;
     tr.appendChild(td);
@@ -76,18 +105,19 @@ var renderRow = function(parentElement) {
 };
 
 // Deletes Rows by ID
-var deleteRow = function(rowid)  {   
-  var row = document.getElementById(rowid);
+// Source: https://stackoverflow.com/questions/4967223/delete-a-row-from-a-table-by-id
+var deleteRow = function(rowId){
+  var row = document.getElementById(rowId);
   row.parentNode.removeChild(row);
 };
 
 // Object and Prototypes for stores
-function Store(storeLocation, minCustomers, maxCustomers, avgCookiesPerCustomer, cookiesPurchasePerHour) {
+function Store(storeLocation, minCustomers, maxCustomers, avgCookiesPerCustomer) {
   this.storeLocation = storeLocation;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
-  this.cookiesPurchasePerHour = cookiesPurchasePerHour || 9999;
+  this.cookiesPurchasePerHour = [];
 
   centralOregonStores.push(this);
 }
@@ -97,7 +127,6 @@ Store.prototype.avgCustomersPerHour = function() {
 };
 
 Store.prototype.addToCookiesList = function() {
-  this.cookiesPurchasePerHour = [];
   for (var i = 0; i < 15; i++) {
     this.cookiesPurchasePerHour.push(Math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCustomer));
   }
@@ -130,7 +159,7 @@ renderRowOfTimes(tableOfSales);
 //-------------------------
 // Event handlers
 //-------------------------
-var CityForm = document.getElementById('addCityForm');
+var cityForm = document.getElementById('addCityForm');
 
 var addCityEventHandler = function(event){
   event.preventDefault();
@@ -140,20 +169,19 @@ var addCityEventHandler = function(event){
     deleteRow('totals');
   }
 
+  var target = event.target;
+  var name = target.cityName.value;
+  var min = parseInt(target.min.value, 10);
+  var max = parseInt(target.max.value, 10);
+  var avg = parseInt(target.avg.value, 10);
 
-  var name = event.target.cityName.value;
-  var min = parseInt(event.target.min.value, 10);
-  var max = parseInt(event.target.max.value, 10);
-  var avg = parseInt(event.target.avg.value, 10);
-
-  event.target.reset();
+  target.reset();
 
   var city = new Store(name, min, max, avg);
   city.render(tableOfSales);
 
   renderRowOfTotals(tableOfSales, centralOregonStores);
-
 };
 
-CityForm.addEventListener('submit', addCityEventHandler);
+cityForm.addEventListener('submit', addCityEventHandler);
 
